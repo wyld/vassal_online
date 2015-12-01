6b6726d0dd602340dd9a8e93eaaef5437fe751b0
Alexander Rozdymakha <alex.rozdy@gmail.com>
2015-12-01 21:02:27 +0200

    fixed caching

--- webapp/utils/cache.py
+++ webapp/utils/cache.py
@@ -1,6 +1,7 @@
 import asyncio
-#import aioredis
+import aioredis
 import hashlib
+from functools import wraps
 import ujson
 
 import settings
@@ -11,31 +12,42 @@ loop = asyncio.get_event_loop()
 
 class get_connection(object):
     async def __aenter__(self):
-        return await aioredis.create_redis(
+        self.redis = await aioredis.create_redis(
             (settings.CACHE_HOST, settings.CACHE_PORT), loop=loop)
+        return self.redis
 
     async def __aexit__(self, type, value, traceback):
         self.redis.close()
 
 
 async def read_cache(key):
-    with get_connection() as redis:
+    async with get_connection() as redis:
         return await redis.get(key)
 
 
 async def write_cache(key, value):
-    with get_connection() as redis:
+    async with get_connection() as redis:
         return await redis.set(key, value)
 
 
-CACHE = {}
+async def clear_cache(key):
+    async with get_connection() as redis:
+        return await redis.delete(key)
 
 
 def cache_per_context(func):
+    @wraps(func)
     async def _wrapper(template, request, context, **kwargs):
-        key = hashlib.md5(ujson.dumps(context).encode('utf-8')).digest()
-        value = CACHE.get(key)
+        key = '{}={}'.format(
+            func.__name__, 
+            hashlib.md5(ujson.dumps(context).encode('utf-8')).digest())
+
+        value = await read_cache(key)
+        if isinstance(value, bytes):
+            value = value.decode('utf-8')
+
         if not value:
-            CACHE[key] = value = func(template, request, context, **kwargs)
+            value = func(template, request, context, **kwargs)
+            await write_cache(key, value)
         return value
     return _wrapper
